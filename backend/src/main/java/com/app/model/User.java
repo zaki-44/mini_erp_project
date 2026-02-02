78875edf54e3984ed440ba11c94df57fa84a9fa5
ACEECA1 <walidchemat@gmail.com>
2026-02-02 22:42:18 +0100

    added transient to passwordHash to not send it in the response

--- backend/src/main/java/com/app/model/User.java
+++ backend/src/main/java/com/app/model/User.java
@@ -6,7 +6,7 @@ public class User {
     private int id;
     private String email;
     private String username;
-    private String passwordHash;
+    private transient String passwordHash;
     private String firstName;
     private String lastName;
     private String phoneNumber;
